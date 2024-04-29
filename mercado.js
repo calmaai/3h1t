a848f5300214caf95d4ce18f1fcafdbf9323b280
guastalinha <110241549+calmaai@users.noreply.github.com>
2024-04-29 12:36:54 -0300

    Add files via upload

---
+++ mercado.js
@@ -0,0 +1,29 @@
+const Fila = () => {
+    let dado = {};
+    const enfileirar = valor =>{
+        imprimir();
+    }
+    const tamanho = () => dados.length;
+    const estaVazia = () => dados.length < 1;
+    const desenfileirar = () => {
+        if(estaVazia){
+            dados.push(0,1);
+            imprimir();
+        }
+    }
+    const imprimir = () => {
+        console.log(dados);
+    }
+    return{
+        enfileirar,
+        desenfileirar,
+        tamanho
+    }
+}
+const filaUnica = Fila();
+const numItens = () => Math.floor(Math.random() * (15-1) + 1);
+(function(){
+    for (let i=0; i<15; i++){
+        filaUnica.enfileirar({id:"p"+f1.tamanho()+1, itens:numItens()});
+    }
+})();
